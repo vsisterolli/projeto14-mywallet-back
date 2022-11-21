51e07af82a10e8c9dedb79b75e547381b6dd3798
vsisterolli <vsisterolli@gmail.com>
2022-11-21 14:57:14 -0300

    chore: adjust code for deploy on heroku

--- src/database/db.js
+++ src/database/db.js
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config()
 
-const mongoClient = new MongoClient(process.env.MONGO_URL)
+const mongoClient = new MongoClient(process.env.MONGO_URI)
 try {
     await mongoClient.connect()
     console.log("Succesfully connected to database")
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import usersRouter from "./routes/users.routes.js";
 import registerRouter from "./routes/register.routes.js";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express()
 const router = express.Router()
@@ -13,4 +15,4 @@ app.use(express.json())
 app.use(cors())
 app.use(router)
 
-app.listen(5000)
+app.listen(process.env.PORT)
